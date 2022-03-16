78afa9b80196012b35af7add0b4dc896686dc86b
KrisLopez98 <87205060+KrisLopez98@users.noreply.github.com>
2022-03-15 18:24:05 -0600

    Marcadores

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,16 +1,88 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
+// tslint:disable-next-line:class-name
+interface colorMarcadores {
+  color: string;
+  marker: mapboxgl.Marker;
+}
 @Component({
   selector: 'app-marcadores',
   templateUrl: './marcadores.component.html',
   styles: [
+    `
+    .mapa-container {
+      width: 100%;
+      height: 100%;
+    }
+
+    .list-group {
+      position: fixed;
+      top: 20px;
+      right: 20px;
+      z-index: 99;
+    }
+
+    li {
+      cursor: pointer;
+    }
+  `
   ]
 })
-export class MarcadoresComponent implements OnInit {
+export class MarcadoresComponent implements AfterViewInit {
+
+  @ViewChild('mapa') divMapa!: ElementRef;
+  mapa!: mapboxgl.Map;
+  zoomLevel = 15;
+  center: [number, number] = [ -97.41707739786288, 18.468854714273494 ];
+
+  // Arreglo de marcadores
+  marcadores: colorMarcadores[] = [];
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.center,
+      zoom: this.zoomLevel
+    });
+
+    // Marcador personalizado
+    // const marketHtml: HTMLElement = document.createElement('div');
+    // marketHtml.innerHTML = 'HolaMundo';
+
+    const markerOne = new mapboxgl.Marker()
+      .setLngLat(this.center)
+      .addTo(this.mapa);
   }
 
+
+  // Agregar multiples marcadores
+  agregarMarcador(): void {
+    // tslint:disable-next-line:no-bitwise
+    const color = '#xxxxxx'.replace(/x/g, y => (Math.random() * 16 | 0).toString(16));
+
+    const nuevoMarcador = new mapboxgl.Marker({
+      draggable: true,
+      color
+    })
+    .setLngLat(this.center)
+    .addTo(this.mapa);
+
+    // Se agregan los nuevo marcadores
+    this.marcadores.push({
+      color,
+      marker: nuevoMarcador
+    });
+  }
+
+
+  // Obtener la longitud y latitud de un marcador
+  irMarcador(marker: mapboxgl.Marker): void {
+    this.mapa.flyTo({
+      center: marker.getLngLat()
+    });
+  }
 }
--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
 @Component({
@@ -19,24 +19,48 @@ import * as mapboxgl from 'mapbox-gl';
       border-radius: 4px;
       position: fixed;
       z-index: 9999;
+      width:40%;
     }
     `
   ]
 })
-export class ZoomRangeComponent implements AfterViewInit {
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy {
 
   @ViewChild('mapa') divMapa!: ElementRef;
-
   mapa!: mapboxgl.Map;
+  zoomLevel = 10;
+  center: [number, number] = [ -97.41707739786288, 18.468854714273494 ];
 
   constructor() { }
+  ngOnDestroy(): void {
+    this.mapa.off('zoom', () => {});
+    this.mapa.off('zoomend', () => {});
+    this.mapa.off('move', () => {});
+  }
 
   ngAfterViewInit(): void {
     this.mapa = new mapboxgl.Map({
       container: this.divMapa.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [ -97.41707739786288, 18.468854714273494 ],
-      zoom: 17
+      center: this.center,
+      zoom: this.zoomLevel
+    });
+    // EVENTLISTENER DEL MAPA Ayuda para saber cuando el zoom cambia
+    this.mapa.on('zoom', (evento) => {
+      this. zoomLevel = this.mapa.getZoom();
+    });
+    // EVENTLISTENER: Funciona para obtener un maximo en el zoom
+    this.mapa.on('zoomend', (ev) => {
+      if (this.mapa.getZoom() > 20){
+        this.mapa.zoomTo(20);
+      }
+    });
+    // EVENTLISTENER: Funciona para obtener la data(valor) de la longitud y latitud en
+    // -------------- cuanto cambie zoom MOVIMIENTO DEL MAPA
+    this.mapa.on('move', (evento) => {
+      const target = evento.target;
+      const {lng, lat} = target.getCenter();
+      this.center = [ lng, lat];
     });
   }
 
@@ -45,7 +69,12 @@ export class ZoomRangeComponent implements AfterViewInit {
   }
 
   zoomOut(): void {
-    this.mapa.zoomOut();
+  this.mapa.zoomOut();
+  }
+
+  // Obtener el valor cuando este cambia
+  zoomChange(valor: string): void {
+    this.mapa.zoomTo(Number(valor));
   }
 
 }
